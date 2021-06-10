4ba2a473051505916ea5e9ad80b9cd936645eae3
cetherdev <robert.hct@gmail.com>
2021-06-10 13:50:18 -0500

    First commit

---
+++ app/src/main/java/com/glass/siiga/conexion/Conectar_HttpPost.java
@@ -0,0 +1,160 @@
+package com.glass.siiga.conexion;
+
+import android.app.Activity;
+import android.os.StrictMode;
+import android.util.Log;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by Arturo on 2/10/2018.
+ */
+
+public class Conectar_HttpPost {
+
+    String responseServer, url;
+    JSONObject jsonRespuesta;
+    Conectar_HttpsPost conectar;
+
+    public JSONObject enviarParametros(String url, HashMap<String, String> listado, Activity context){
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        this.url = url;
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost("https://siiges/controllers/"+url);
+
+        try {
+            List<NameValuePair> nameValuePairs = new ArrayList<>();
+            Set keys = listado.keySet();
+
+            for (Iterator i = keys.iterator(); i.hasNext(); ) {
+                String key = (String) i.next();
+                String value = listado.get(key);
+
+                nameValuePairs.add(new BasicNameValuePair(""+key, ""+value.replace("%20", " ").replace("%0A", "<br>")));
+                //Log.e(key , value);
+            }
+
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs, "UTF-8"));
+            HttpResponse response = httpclient.execute(httppost);
+            InputStream inputStream = response.getEntity().getContent();
+            InputStreamToString str = new InputStreamToString();
+
+            responseServer = str.getStringFromInputStream(inputStream);
+            Log.e("RES", responseServer);
+
+            jsonRespuesta = new JSONObject(responseServer);
+
+        } catch (ClientProtocolException e) {
+            // TODO Auto-generated catch block
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return jsonRespuesta;
+    }
+
+    public static class InputStreamToString {
+        private static String getStringFromInputStream(InputStream is) {
+            BufferedReader br = null;
+            StringBuilder sb = new StringBuilder();
+            String line;
+
+            try {
+                br = new BufferedReader(new InputStreamReader(is));
+                while ((line = br.readLine()) != null) {
+                    sb.append(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (br != null) {
+                    try {
+                        br.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return sb.toString();
+        }
+    }
+
+    public void sendJsonPost(String url_a, JSONObject jsonParam) {
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        try {
+            String result = "";
+
+            URL url = new URL(url_a);
+
+            // 1. create HttpURLConnection
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+
+            // 3. add JSON content to POST request body
+            OutputStream os = conn.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+            writer.write(jsonParam.toString());
+            //Log.i(MainActivity.class.toString(), jsonObject.toString());
+            writer.flush();
+            writer.close();
+            os.close();
+
+            // 4. make POST request to the given URL
+            conn.connect();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            br.close();
+
+            Log.e("JSON", sb.toString());
+            //return sb.toString();
+
+            // 5. return response message
+            //return conn.getResponseMessage()+"";
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
